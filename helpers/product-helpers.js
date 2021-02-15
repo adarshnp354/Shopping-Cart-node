a0c5b047bd7b7334d1d257f8f0937450cf528591
adarshnp354 <74529927+adarshnp354@users.noreply.github.com>
2021-02-15 15:42:10 +0530

    Update product-helpers.js

--- helpers/product-helpers.js
+++ helpers/product-helpers.js
@@ -1,6 +1,7 @@
 var db = require('../config/connection')
 var collection=require('../config/collections')
 var objectId = require('mongodb').ObjectID
+const { response } = require('express')
 module.exports={
 
     addProduct:(product,callback)=>{
@@ -28,10 +29,25 @@ module.exports={
             })
         })
     },
-    getProduct:(id)=>{
+    getProductDetails:(id)=>{
         return new Promise((resolve,reject)=>{
             db.get().collection(collection.PRODUCT_COLLECTION).findOne({_id:objectId(id)}).then((response)=>{
                 resolve(response)
+            })
+        })
+    },
+    updateProduct:(proId,proDetails)=>{
+        return new Promise((resolve,reject)=>{
+            db.get().collection(collection.PRODUCT_COLLECTION)
+            .updateOne({_id:objectId(proId)},{
+                $set:{
+                    Name:proDetails.Name,
+                    Description:proDetails.Description,
+                    Price:proDetails.Price,
+                    Category:proDetails.Category
+                }
+            }).then((response)=>{
+                resolve()
             })
         })
     }
