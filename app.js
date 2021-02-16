504f27cb8563d0ef523ff81de7c99345789a9eef
adarshnp354 <74529927+adarshnp354@users.noreply.github.com>
2021-02-16 17:39:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload())      ///------> fileupload is used here
 app.use(session({secret:"Key",cookie:{maxAge:600000}}))
+
 db.connect((err)=>{
   if(err){
     console.log("Connection Error");
